6dca68425feae9aa3f37db36abdd3c48e0f0305d
Amir Shammasov <sammasovamir29@gmail.com>
2021-12-09 18:13:32 +0300

    Added authentication

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PageController extends Controller
+{
+    public function index() {
+        $user = Auth::user();
+        return view('pages.home', compact('user'));
+    }
+
+    public function results() {
+        $user = Auth::user();
+        return view('pages.results', compact('user'));
+    }
+}
